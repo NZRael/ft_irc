de3e26ec339275d1b24e0b3c1b8f765f213a95f9
Soheil Boetti <sboetti@c1r7p3.42nice.fr>
2024-10-02 17:40:27 +0200

    Faut voir

--- inc/Bot.hpp
+++ inc/Bot.hpp
@@ -2,12 +2,13 @@
 
 #include "ft_irc.hpp"
 
+#define BONJOUR "Bonjour"
+#define SAL "Salut"
+
 class Bot : public ACommand {
     public:
         Bot();
         ~Bot();
         void start(Client *user) const;
-
         void execute(Client *user, std::string raw_message, Server *server) const;
-
 };
--- srcs/Bot.cpp
+++ srcs/Bot.cpp
@@ -5,8 +5,7 @@ Bot::Bot() : ACommand("BOT") {}
 Bot::~Bot() {}
 
 void Bot::start(Client *user) const {
-
-    user->sendMessage("Bonjour");
+    user->sendMessage(":BOT Bonjour " + user->getNickname() + " !");
 }
 
 
@@ -14,6 +13,8 @@ void Bot::execute(Client *user, std::string raw_message, Server *server) const{
 	if (raw_message.empty())
 	    return;
 	(void)server;
-    if (raw_message == "bonjour")
+    if (raw_message == BONJOUR || raw_message == SAL)
         start(user);
+	else
+		user->sendMessage(":BOT Je ne comprends pas ce que vous voulez dire.");
 }
